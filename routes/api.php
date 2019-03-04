2141f566eb91c56b6af8eafa7b58b50c7224df26
Artem Yarygin <artx19@yandex.ru>
2019-03-04 18:03:52 +0300

    user register with test

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['json','auth:api'])->group(function(){
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+Route::middleware(['json'])->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
+        Route::resource('items', 'ItemController')->only('index', 'show', 'store', 'destroy');
     });
-
-    Route::resource('items', 'ItemController');
+    Route::post('users', 'UserController@store');
 });
+
 
