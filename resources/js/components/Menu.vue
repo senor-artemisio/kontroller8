0bc4f2a9ab36d65dd49f691bc275a83b0e141a96
Artem Yarygin <artx19@yandex.ru>
2019-03-20 21:52:22 +0300

    something...

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -1,36 +1,66 @@
 <template>
-        <b-navbar toggleable="sm" type="dark" variant="primary">
-            <b-navbar-brand>Kontroller8</b-navbar-brand>
+    <b-navbar toggleable="sm" type="dark" variant="primary">
+        <b-navbar-brand>Kontroller8</b-navbar-brand>
 
-            <b-navbar-toggle target="nav_collapse" />
+        <b-navbar-toggle target="nav_collapse"/>
 
-            <b-collapse is-nav id="nav_collapse">
-                <b-navbar-nav>
-                    <b-nav-item to="/dashboard">Dashboard</b-nav-item>
-                    <b-nav-item to="/items">Items</b-nav-item>
-                </b-navbar-nav>
+        <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav>
+                <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+                <b-nav-item to="/items">Items</b-nav-item>
+            </b-navbar-nav>
 
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-                    <b-nav-form>
-                        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" />
-                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                    </b-nav-form>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                    <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
 
-                    <b-nav-item-dropdown text="Lang" right>
-                        <b-dropdown-item href="#">EN</b-dropdown-item>
-                        <b-dropdown-item href="#">ES</b-dropdown-item>
-                        <b-dropdown-item href="#">RU</b-dropdown-item>
-                        <b-dropdown-item href="#">FA</b-dropdown-item>
-                    </b-nav-item-dropdown>
+                <b-nav-item-dropdown text="Lang" right>
+                    <b-dropdown-item href="#">EN</b-dropdown-item>
+                    <b-dropdown-item href="#">ES</b-dropdown-item>
+                    <b-dropdown-item href="#">RU</b-dropdown-item>
+                    <b-dropdown-item href="#">FA</b-dropdown-item>
+                </b-nav-item-dropdown>
 
-                    <b-nav-item-dropdown right>
-                        <!-- Using button-content slot -->
-                        <template slot="button-content"><em>User</em></template>
-                        <b-dropdown-item href="#">Profile</b-dropdown-item>
-                        <b-dropdown-item href="#">Signout</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
+                <b-nav-item-dropdown right>
+                    <template slot="button-content"><b>{{ user.name }}</b></template>
+                    <b-dropdown-item href="#">Profile</b-dropdown-item>
+                    <b-dropdown-item v-on:click="signOut()">Signout</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
 </template>
+
+<script>
+    import api from '../api';
+    import Cookies from 'js-cookie';
+
+    export default {
+        data() {
+            return {
+                user: {
+                    name: "..."
+                }
+            };
+        },
+        mounted() {
+            api.get('users/me').then(response => {
+                if (response.data) {
+                    this.user = response.data.data;
+                } else {
+                    throw response;
+                }
+            }).catch(error => {
+                this.$router.push({name: 'auth'});
+            });
+        },
+        methods: {
+            signOut() {
+                Cookies.remove('X-AUTH-TOKEN');
+                this.$router.push({name: 'auth'});
+            }
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 $routes = [
     '/',
     '/dashboard',
+    '/items',
     '/auth',
 ];
 
